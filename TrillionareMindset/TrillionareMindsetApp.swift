71818cecc4cf4b20a2c443187634a8e94e9157ae
Jai <jai07rajput@gmail.com>
2024-05-25 17:14:35 +0530

    Project-setup

---
+++ TrillionareMindset/TrillionareMindsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrillionareMindsetApp.swift
+//  TrillionareMindset
+//
+//  Created by Jai  on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TrillionareMindsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
